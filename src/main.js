c7a97d1bbf7d41fe72559f47a3c0471e18d840c6
Howyna K <93727365+13-1550@users.noreply.github.com>
2026-05-06 16:14:33 +0300

    Updates

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-import { createApp } from 'vue';
-import PortfolioPage from './PortfolioPage.vue';
-
-// Create the Vue application
-const app = createApp(PortfolioPage);
-app.mount('#app');
